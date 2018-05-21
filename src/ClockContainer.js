3ce213aec7acce3a8c8120a808558c53d86344df
Laura Brandt <laurabrandt@Lauras-MacBook-Pro.local>
2018-05-21 11:52:11 -0400

    moved values to state

--- src/ClockContainer.js
+++ src/ClockContainer.js
@@ -5,6 +5,16 @@ import Controls from './Components/Controls/Controls';
 import './ClockContainer.css';
 
 class ClockContainer extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      sessionLength: 25,
+      breakLength: 5,
+      timeLeft: 25*60
+    }
+  }
+
   render() {
     return (
       <div className="clock-container">
@@ -12,10 +22,10 @@ class ClockContainer extends React.Component {
           <h1>Pomodoro Clock</h1>
         </header>
         <div className="settings">
-          <TimeSetting type="Session" length={25} />
-          <TimeSetting type="Break" length={5} />
+          <TimeSetting type="Session" length={this.state.sessionLength} />
+          <TimeSetting type="Break" length={this.state.breakLength} />
         </div>
-        <TimeDisplay time="25:00" type="Session"/>
+        <TimeDisplay time={this.state.timeLeft} type="Session"/>
         <Controls />
         <footer>Designed and coded by <a href="https://github.com/LauraBrandt">Laura Brandt</a></footer>
       </div>
